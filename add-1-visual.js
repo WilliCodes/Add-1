bce45f13836b04769847f390e05bd4ad81288a8f
Jan Zickerick <19734461+WilliCodes@users.noreply.github.com>
2019-02-15 13:31:31 +0100

    add visual only mode

---
+++ add-1-visual.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+    var toGuess = [];
+
+    var number = document.getElementById('number');
+
+    var inputCounter = 0;
+
+
+    setInterval(function() {
+        if (toGuess.length < 4) {
+            let newNum = Math.floor(Math.random() * 10);
+            number.innerHTML = newNum.toString();
+            toGuess.push(newNum);
+        } else if (inputCounter === 0){
+            inputCounter++;
+            number.innerHTML = '#';
+        } else if (inputCounter < 4) {
+            // check each input
+            inputCounter++;
+            number.innerHTML = '-'.repeat(inputCounter-1);
+        } else if (inputCounter === 4) {
+            inputCounter++;
+            number.innerHTML = '----';
+        } else if (inputCounter < 7) {
+            inputCounter++;
+            var correct = '';
+            for (var i = 0; i < toGuess.length; i++)
+                correct += ((toGuess[i] + 1) % 10).toString();
+            number.innerHTML = correct;
+        } else {
+        inputCounter = 0;
+        toGuess = [];
+        number.innerHTML = '!';
+    }
+    }, 1000);
+
+};
