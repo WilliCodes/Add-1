4b5e4dc73168cd56abe1c94e88f8894b99130567
Jan Zickerick <19734461+WilliCodes@users.noreply.github.com>
2019-02-15 01:32:25 +0100

    complete without error checking

---
+++ game.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+    var toGuess = [];
+
+    var number = document.getElementById('number');
+    var input = document.getElementById('input');
+
+    var inputCounter = 0;
+
+
+    setInterval(function() {
+        if (toGuess.length < 4) {
+            let newNum = Math.floor(Math.random() * 10);
+            number.innerHTML = newNum.toString();
+            toGuess.push(newNum);
+        } else if (inputCounter === 0){
+            inputCounter++;
+        } else if (inputCounter < 4) {
+            // check each input
+            inputCounter++;
+        } else {
+            console.log(toGuess.toString());
+            console.log(input.value);
+            // check if all correct
+            inputCounter = 0;
+            toGuess = [];
+        }
+    }, 1000);
+};
